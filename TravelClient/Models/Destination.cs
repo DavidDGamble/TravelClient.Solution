ddb9358df15af8d38df3d3bd13163080df401a58
davddgamble <daviddgamble@gmail.com>
2023-01-18 17:01:06 -0800

    Add initial code.

---
+++ TravelClient/Models/Destination.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TravelClient.Models
+{
+  public class Destination
+  {
+    public int DestinationId { get; set; }
+    public string Name { get; set; }
+
+    public List<Review> Reviews { get; set; }
+
+    public static List<Destination> GetDestination()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Destination> destinationList = JsonConvert.DeserializeObject<List<Destination>>(jsonResponse.ToString());
+
+
+      return destinationList;
+
+    }
+
+    public static Destination GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Destination destination = JsonConvert.DeserializeObject<Destination>(jsonResponse.ToString());
+
+      return destination;
+    }
+
+    public static void Post(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      ApiHelper.Post(jsonDestination);
+    }
+        public static void Put(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      ApiHelper.Put(destination.DestinationId, jsonDestination);
+    }
+
+    public static void Delete(int id)
+    {
+      ApiHelper.Delete(id);
+    }
+  }
+}
---
+++ TravelClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelClient.Models;
+#nullable enable
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
